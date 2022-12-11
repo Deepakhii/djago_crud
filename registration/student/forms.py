3db01ccd7da9bce0f51185e24b6c5afd233a6ff9
Deepak Sharma <75806982+dipu-developer@users.noreply.github.com>
2022-12-11 10:30:23 +0530

    removing unwanted comment
    removing unwanted changes in the forms.py file

--- registration/student/forms.py
+++ registration/student/forms.py
@@ -1,37 +1,5 @@
 from django import forms
 from . import models
-
-
-
-# class StudentRegForms(forms.Form):
-#     ENDER_CHOICES = (
-#         ('M', 'Male'),
-#         ('F', 'Female'),
-#     )
-#     name= forms.CharField(max_length=200,widget=forms.TextInput(attrs={'class':"form-control"}))
-#     email   = forms.EmailField(widget=forms.EmailInput(attrs={'class':"form-control"}))
-#     mobile  = forms.IntegerField(widget=forms.NumberInput(attrs={'class':"form-control input-group"}))
-#     gender  = forms.CharField(max_length=1, label="Gender",
-#                                 initial='M',
-#                                 widget=forms.Select(choices=ENDER_CHOICES,attrs={'class':"form-control form-control"}),
-#                                 required=True)
-#     dob     = forms.DateField(widget=forms.DateInput(attrs={'type':'date','class':"form-control"}))
-#     file    = forms.FileField(widget=forms.FileInput(attrs={'class':"form-control"}))
-#     password = forms.CharField(max_length=200,widget=forms.PasswordInput(attrs={'class':"form-control"}))
-
-#     class Meta:
-#         model= models.StudentReg
-#         fields = ['name','email','mobile','gender']
-
-# class StudentRegform(forms.ModelForm):
-    
-
-# class MyForm(forms.ModelForm):
-#   class Meta:
-#     model = MyModel
-#     fields = ["fullname", "mobile_number",]
-#     labels = {'fullname': "Name", "mobile_number": "Mobile Number",}
-
 class StudentRegForms(forms.ModelForm):
     ENDER_CHOICES = (
         ('Male', 'Male'),
